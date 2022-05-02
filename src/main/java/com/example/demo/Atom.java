57e67b509daf831d39b684346bd86162cd5924cb
Michael Turner <michael.eugene.turner@gmail.com>
2022-05-02 21:35:09 +0900

    Comment cleanups

--- src/main/java/com/example/demo/Atom.java
+++ src/main/java/com/example/demo/Atom.java
@@ -92,21 +92,12 @@ public class Atom {
         Valence this_ = Valence.of(Nucleus.THIS,null,null);
         Valence maybe = Valence.of(Nucleus.MAYBE, null, null);
 
-        // S = true_;
         S = map (true_,true_, "S"); // for now
         
-        // S = O_(say,this_);   // or someone can think this?
-
-        //
-        // Valence maybe = Valence.of (Nucleus.MAYBE,null,null);
-        // Pred = Valence.of (Nucleus.MAYBE,null,null,"Pred");
-        Pred = map (maybe,maybe, "Pred");
+        Pred = map (maybe,maybe, "Pred");  // for now
+        // Pred = O_(O_(maybe,true_),this_); // Predicate something that may be true about something/someone
 
         PredS = map (Pred, S, "PredS");
-
-        // Pred = maybe;
-
-        // Pred = O_(O_(maybe,true_),this_); // Predicate something that may be true about something/someone
 
         Subst = map (PredS, S, "Subst");    // iffy
 
@@ -120,21 +111,19 @@ public class Atom {
         Someone = someone;
         Something = something;
         //
-        // still no O_(maybe,true)
+        // still no O_(Pred,S)
         // this is why it goes into default
         //
 
-/* I */
+// I
         TreeSet<Valence> I = valences_for("I");
-////        I.add (O_(say, S));
         I.add (someone);
-////        I.add (O_(p1, S)); // too general, but anyway
 
-/* YOU, */
+// YOU
         TreeSet<Valence> you = valences_for("YOU");
         you.add (someone);
 
- /*  SOMEONE, */
+ // SOMEONE
         TreeSet<Valence> so = valences_for("SOMEONE");
         so.add(O_(is,Pred));
         so.add(O_(Pred, S)); // someone can be good, bad; do thing, etc.
@@ -142,12 +131,9 @@ public class Atom {
         so.add(O_(bad,someone));
         so.add(O_(good,someone));
         so.add(O_(this_,someone));
-//        valences_for("SOMEONE").add(O_(O_(say,true_),say)); // someone can say something maybe true
-//        valences_for("SOMEONE").add(O_(live,true_));
-/*
-   SOMETHING, */
+
+// SOMETHING
         TreeSet<Valence> st = valences_for ("SOMETHING");
-//        st.add (O_(say,O_(say,true_)));   // can say something, though a something can't say things
         st.add(O_(is,Pred));
         st.add(O_(Pred,S));
         st.add(O_(bad,something)); // something that's bad is still something
@@ -159,7 +145,7 @@ public class Atom {
    PART,
    WORDS,
 */
-//   THIS,
+// THIS
         TreeSet<Valence> a_this = valences_for("THIS");
         a_this.add(O_(something,something));
         a_this.add(O_(someone,someone));
@@ -173,8 +159,7 @@ public class Atom {
    ALL,
 */
 
-//   SOME,
-/*
+/* SOME,
    LITTLE,
 
    TIME_when,
@@ -187,12 +172,12 @@ public class Atom {
    BEFORE,
    AFTER,
 */
-//   WANT,
+// WANT
         TreeSet<Valence> how_to_want = valences_for ("WANT");
         how_to_want.add(O_(something, Pred));
         how_to_want.add(O_(this_,Pred));
 
-//   DONT_WANT,
+// DONT_WANT
         TreeSet<Valence> how_to_not_want = valences_for ("DONT_WANT");
         how_to_not_want.add(O_(something, Pred));       // ???
         how_to_not_want.add(O_(this_,Pred));
@@ -200,51 +185,57 @@ public class Atom {
 /*
    FEEL,
 */
-//   DO,
+// DO
         TreeSet<Valence> how_to_do = valences_for ("DO");
         how_to_do.add (O_(something,Pred));
-//   SAY,
+
+// SAY
         TreeSet<Valence> how_to_say = valences_for ("SAY");
 //        how_to_say.add (O_(something, say)); // just raw SOMETHING, but need SOMETHING LIKE WORD(S)
         how_to_say.add (O_(S, Pred));       // for actual statement, maybe need a "MAYBE TRUE"
         how_to_say.add (O_(something,Pred));    // "SAY SOMETHING", but dangerous: could mean you could say an object
-//        how_to_say.add (O_(live, say));
-////        how_to_say.add (O_(someone, S));
-/* KNOW, */
+
+// KNOW
         TreeSet<Valence> how_to_know = valences_for ("KNOW");
         how_to_know.add (O_(something, Pred));
         how_to_know.add(Pred); // e.g., "I [NOT/don't] KNOW"/"I KNOW" ???
 /*
    SEE,
    HEAR,
-   THINK, */ 
+   THINK,
+*/ 
+
 ////        valences_for ("THINK").add (p1);
+
 // HAPPEN,
         TreeSet<Valence> how_to_happen = valences_for("HAPPEN");
         how_to_happen.add(O_(someone,Pred));   // happen TO someone
         how_to_happen.add(Pred);        // too broad -- a person can't happen to you, for example
-//   IS,    // copula not BE (SOMEWHERE) */
-        TreeSet<Valence> is_ = valences_for("BE");
+
+// BE    // copula not BE (SOMEWHERE) */
+        TreeSet<Valence> be = valences_for("BE");
 ////        is.add (O_(someone,p1));
-        is_.add (O_(something,Pred));
-        is_.add (O_(someone,Pred));             // may need to distinguish person IS from thing IS
+        be.add (O_(something,Pred));
+        be.add (O_(someone,Pred));             // may need to distinguish person IS from thing IS
 ////        is.add (cop);                   // ???????????
-        is_.add(O_(good,Pred));
-        is_.add(O_(bad,Pred));
-//        is_.add(O_(someone,is));  // Makes "someone is"->S, but it will be discarded as nonsensical unless alone
-//        is_.add(O_(say,is)); // e.g. say (someone is good) -> someone is saying an "is"
-/* LIVE, */
+        be.add(O_(good,Pred));
+        be.add(O_(bad,Pred));
+//        be.add(O_(someone,is));  // Makes "someone is"->S, but it will be discarded as nonsensical unless alone
+//        be.add(O_(say,is)); // e.g. say (someone is good) -> someone is saying an "is"
+
+// LIVE
  //       valences_for("LIVE").add(O_(someone,live));
         valences_for("LIVE").add(Pred);
-/*
-   DIE, */
+
+// DIE
         valences_for("DIE").add(Pred);
 /*
    THERE_IS,
-   BE,    // ... somewhere */
-////        TreeSet<Type> be = valences_for("BE");
-////        be.add (O_(something,somewhere)); // was p1
-////        be.add (O_(someone,somewhere));
+   BE_SOMEWHERE,
+////        TreeSet<Type> be_sw = valences_for("BE");
+////        be_sw.add (O_(something,somewhere)); // was p1
+////        be_sw.add (O_(someone,somewhere));
+
 /* IS_MINE,
    MOVE,
    TOUCH,
@@ -258,16 +249,18 @@ public class Atom {
    NEAR,
    FAR,
 */
-//   NOT,
+// NOT
         valences_for ("NOT").add(ModPred);
 
-//   CAN,
+// CAN
         valences_for("CAN").add(ModPred);    // too broad, just a start
-/*
-   BECAUSE, */
+
+//   BECAUSE, */
 ////        valences_for("BECAUSE").add(osc);
-// IF,
+
+// IF
         valences_for ("IF").add(O_(S,Cond)); // guessing from Haskell code
+
 /* MAYBE,
    LIKE,
    VERY,
@@ -275,16 +268,18 @@ public class Atom {
 
    SMALL,
    BIG,
-   BAD, */
+*/
+
+// BAD
         TreeSet<Valence> how_to_be_bad = valences_for ("BAD");
         how_to_be_bad.add(O_(something,something));
         how_to_be_bad.add(O_(someone,someone));
 
-////                valences_for("BAD").add(O_(is,O_(is, bad)));
-/* GOOD, */
+// GOOD
         TreeSet<Valence> how_to_be_good = valences_for ("GOOD");
         how_to_be_good.add(O_(something,something));
         how_to_be_good.add(O_(someone,someone));
+
 /* TRUE
 */
 
